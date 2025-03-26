edf7ba0f7a301dcaa72edfb0a21a0dbe65410555
Leon1713 <61048222+Leon1713@users.noreply.github.com>
2025-03-26 18:52:01 +0800

    Asteroid structure done

--- CSD1130_Asteroids/Include/AsteroidData.h
+++ CSD1130_Asteroids/Include/AsteroidData.h
@@ -4,9 +4,32 @@
 
 struct AsteroidData
 {
+	int owner;
 	AEVec2 position;
 	AEVec2 velocity;
 	AEVec2 direction;
+	int scoreCount;
+
+	
 };
+
+void SetOwner(AsteroidData* data, int owner);
+int GetOwner(AsteroidData* data);
+
+void SetPosition(AsteroidData* data, AEVec2 position);
+AEVec2 GetPosition(AsteroidData* data);
+
+void SetVelocity(AsteroidData* data, AEVec2 velocity);
+AEVec2 GetVelocity(AsteroidData* data);
+
+void SetDirection(AsteroidData* data, AEVec2 direction);
+AEVec2 GetDirection(AsteroidData* data);
+
+void SetScoreCount(AsteroidData* data, int scoreCount);
+int GetScoreCount(AsteroidData* data);
+
+void CalculateLinearConvergent(::AsteroidData* data, float time);
+
+
 
 #endif
